07b521fb3243e470df3b2645519d3b89fb7241be
Khensani Makhubela <Khensani.Makhubela@excellerate.co.za>
2026-03-09 14:15:37 +0200

    included the requirements list

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,14 +85,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME', 'greenlight'),
+#         'USER': os.getenv('DB_USER', 'greenlight_user'),
+#         'PASSWORD': os.environ['DB_PASSWORD'],
+#         'HOST': os.getenv('DB_HOST', 'localhost'),
+#         'PORT': os.getenv('DB_PORT', '5432'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'greenlight'),
-        'USER': os.getenv('DB_USER', 'greenlight_user'),
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
